61a63aa555bfb65dc34a272f04168ff6bf73b962
hugo-rc <39688936+hugo-rc@users.noreply.github.com>
2020-12-18 14:09:54 +0100

    Update resol_num_eq_simpl.py

--- resol_num_eq_simpl.py
+++ resol_num_eq_simpl.py
@@ -17,14 +17,16 @@ C=1
 Tmax=1000
 rmax=1000
 
+w=1 # injection
+
 def iter_h(h,n,i):
-    """n : indice spatial, t : indice temporel"""
+    """i : indice spatial, n : indice temporel"""
     
-    h[n,i+1]=h[n,i]+C**2+delta_t/(n*delta_r**2)*(h[n+1,i]**3*(n+1)*(h[n+1,i]-h[n,i])-h[n,i]**3*n*(h[n,i]-h[n-1,i]))
+    h[n,i+1]=h[n,i]+C**2+delta_t/(n*delta_r**2)*(h[n+1,i]**3*(n+1)*(h[n+1,i]-h[n,i])-h[n,i]**3*n*(h[n,i]-h[n-1,i]))+w
     
 
 def iter_theta(theta,n,i):
-    """n : indice spatial, t : indice temporel"""
+    """i : indice spatial, n : indice temporel"""
 
     theta[n,i+1]=(1/h[n+1,i]**3)*(theta[n,i]*h[n,i]**3+(1/B)*(A*(delta_t/(n*delta_r**2))*((n+1)*(h[n+1,i]-h[n,i])*theta[n+1,i]*h[n+1,i]**5-n*(h[n,i]-h[n-1,i])*theta[n,i]*h[n,i]**5)-C*h[n,i]*theta[n,i]))
     
